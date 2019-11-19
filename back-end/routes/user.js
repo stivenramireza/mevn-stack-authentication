a32f4c46d8f945925bd054f2f66b3ed462c2b437
Stiven Ramírez Arango <sramir70@eafit.edu.co>
2019-11-19 09:51:38 -0500

    Adding PUT route

--- back-end/routes/user.js
+++ back-end/routes/user.js
@@ -3,8 +3,12 @@ const router = express.Router();
 
 import User from '../models/user';
 
+// Hash Password
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
+
+// Filter fields of PUT
+const _ = require('underscore');
 
 // POST
 router.post('/new-user', async(req, res) => {
@@ -26,5 +30,24 @@ router.post('/new-user', async(req, res) => {
         })
     }
 });
+
+// PUT
+router.put('/user/:id', async(req, res) => {
+    const _id = req.params.id;
+    const body = _.pick(req.body, ['name', 'email', 'password', 'active']);
+
+    if(body.password){
+        body.password = bcrypt.hashSync(req.body.password, saltRounds);
+    }
+    try {
+        const userDB = await User.findByIdAndUpdate(_id, body, {new: true, runValidators: true});
+        res.json(userDB);
+    } catch (error) {
+        return res.status(400).json({
+            message: 'An error has occurred',
+            error
+        })
+    }
+})
 
 module.exports = router;
